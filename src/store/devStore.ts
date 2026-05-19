51d73aa3c88ea89f8771edc7515e7eb298570a2e
chuck <532691300@qq.com>
2026-05-19 21:25:31 +0900

    feat(devStore): 增加 showQualityScore 开关字段

--- src/store/devStore.ts
+++ src/store/devStore.ts
@@ -4,16 +4,23 @@ import { setTimeOffset } from "@/lib/time";
 
 interface DevState {
   timeOffset: number;
+  showQualityScore: boolean;
   setTimeOffset: (ms: number) => void;
+  setShowQualityScore: (v: boolean) => void;
   advanceDay: (days: number) => void;
 }
 
 export const useDevStore = create<DevState>((set, get) => ({
   timeOffset: 0,
+  showQualityScore: false,
 
   setTimeOffset: (ms: number) => {
     setTimeOffset(ms);
     set({ timeOffset: ms });
+  },
+
+  setShowQualityScore: (v: boolean) => {
+    set({ showQualityScore: v });
   },
 
   advanceDay: (days: number) => {
